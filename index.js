4d0f905b79f288107bda4f5198e061a68d2d83e9
Virgo <rattana.neak@gmail.com>
2016-04-14 21:04:05 +0000

    Install the body-parser middleware into your application

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-
+var bodyParser = require('body-parser');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -12,6 +15,24 @@ app.get('/', function (req, res) {
 });
 app.get('/about', function (req, res) {
     res.render('about');
+});
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/api/users', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});
+
+app.get('/api/users', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
+
+  res.send('My Get : '+user_id + ' ' + token + ' ' + geo);
 });
 
 app.listen(process.env.PORT);
