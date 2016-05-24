e5c7799b716800b28e8c649c1b28cfc5e3232855
Virgo <rattana.neak@gmail.com>
2016-05-24 16:57:14 +0000

    update test_db.js

--- test_db.js
+++ test_db.js
@@ -1,5 +1,11 @@
-var game = require("./lib/game.js");
-console.log(game.listGame());
+var Game = require("./models/game.js");
+Game.find(function(err,games){
+            if(err) return console.error(err);
+            var listgame = {games : games};
+        
+        //sconsole.log(listgame);
+            console.log(listgame);
+        });
 
 
 
